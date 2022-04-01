88f665e51d54587658748b0b0a0f192b36654414
Martin Rezny <90828916+martin942@users.noreply.github.com>
2022-04-01 11:48:44 +0200

    Add files via upload

---
+++ Functions/Group/Group/Group.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Group
+{
+    public class Group
+    {
+        public string GroupId { get; set; }
+        public string Admin { get; set; }
+        public string Name { get; set; }
+        public string Visibility { get; set; }
+
+        public Group(string groupId, string admin, string name, string visibility)
+        {
+            GroupId = groupId;
+            Admin = admin;
+            Name = name;
+            Visibility = visibility;
+        }
+
+        public Group(string admin, string name, string visibility)
+        {
+            Admin = admin;
+            Name = name;
+            Visibility = visibility;
+            GroupId = Guid.NewGuid().ToString();
+        }
+
+        public Group()
+        {
+        }
+    }
+}
