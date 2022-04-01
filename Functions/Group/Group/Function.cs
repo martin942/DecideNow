581212fc30bfffecce8a6ef8f5476b8009deabe4
Martin Rezny <90828916+martin942@users.noreply.github.com>
2022-04-01 12:26:44 +0200

    Add files via upload

---
+++ Functions/Group/Group/Function.cs
@@ -0,0 +1,86 @@
+using Google.Cloud.Functions.Framework;
+using Microsoft.AspNetCore.Http;
+using MySql.Data.MySqlClient;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Group
+{
+    public class Function : IHttpFunction
+    {
+        
+        public async Task HandleAsync(HttpContext context)
+        {
+            string method = context.Request.Method;
+            switch (method)
+            {
+                case "GET":
+                    await Get(context);
+                    break;
+                case "POST":
+                    await Post(context);
+                    break;
+                case "PATCH":
+                    await Patch(context);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public async Task Get(HttpContext context)
+        {
+
+        }
+
+        public async Task Post(HttpContext context)
+        {
+            string path = context.Request.Path;
+            switch (path)
+            {
+                case "/create":
+                    await PostCreate(context);
+                    break;
+            }
+        }
+
+        public async Task Patch(HttpContext context)
+        {
+
+        }
+
+        public async Task PostCreate(HttpContext context)
+        {
+            string token = ((string)context.Request.Query["token"]);
+            if (token == null || token.Equals(""))
+            {
+                await SendResponseMessage(context, new Message("token missing", 400));
+                return;
+            }
+            string userid = await SqlManager.GetInstance().ValidateToken(token, context.Connection.RemoteIpAddress.ToString());
+            if (userid == null || userid.Equals(""))
+            {
+                await SendResponseMessage(context, new Message("invalid token", 400));
+                return;
+            }
+            // todo
+        }
+
+        private async Task SendResponseMessage(HttpContext context, object responseObject)
+        {
+            string response = JsonSerializer.Serialize(responseObject);
+            context.Response.ContentType = "application/json";
+            if (responseObject.GetType() == typeof(Message))
+            {
+                context.Response.StatusCode = ((Message)responseObject).statusCode;
+            }
+            else
+            {
+                context.Response.StatusCode = 200;
+            }
+            await context.Response.WriteAsync(response);
+        }
+
+    }
+}
---
+++ Functions/Group/Group/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Group
+{
+    public class Message
+    {
+        public object message { get; set; }
+        public int statusCode { get; set; }
+
+        public Message(object message, int statusCode)
+        {
+            this.message = message;
+            this.statusCode = statusCode;
+        }
+    }
+}
